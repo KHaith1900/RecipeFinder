e38e4664e395e403b7e1d747e9ce6f52473d2aeb
KHaith1900 <kendrickhaith@gmail.com>
2024-09-23 14:20:12 -0400

    made introPopup its own method

--- src/main/PantryPalGUI.java
+++ src/main/PantryPalGUI.java
@@ -23,29 +23,40 @@ public class PantryPalGUI {
 
 	/**
 	 * This is the pantryPal method which will create the initial GUI
-	 * to display the instructions and lead to the actual application
+	 * to display the instructions and lead to the actual application.
 	 * 
 	 */
 	public static void pantryPal() {
 		JFrame frame = new JFrame("PantryPal");
+		introPopup();
 		JLabel label = new JLabel("Thanks For Using PantryPal");
 		Image icon = Toolkit.getDefaultToolkit().getImage("./src/images/smallmainlogo.png");
 		frame.setIconImage(icon);
-		Object[] options = { "OK", "Help" };
-		int answer = JOptionPane.showOptionDialog(null,
-				"Welcome to the PantryPal Application, If you are new to the application click the help button, otherwise click ok",
-				"Welcome", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null,
-				options, options[0]);
-		System.out.println("Selected Option: " + answer);
-		if (answer == 1) {
-			help();
-		}
 		frame.add(label);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(500, 500);
 	}
 
+	/**
+	 * This is the introPopup method which shows a popup to the user which directs
+	 * them to either the help section of the application or the main application
+	 */
+	public static void introPopup() {
+		Object[] options = { "OK", "Help" };
+		int answer = JOptionPane.showOptionDialog(null,
+				"Welcome to the PantryPal Application, If you are new to the application click the help button, otherwise click ok",
+				"Welcome", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null,
+				options, options[0]);
+		if (answer == 1) {
+			help();
+		}
+	}
+
+	/**
+	 * This is the help method which will open a message dialog with the
+	 * instructions on how to use the pantryPal GUI.
+	 */
 	public static void help() {
 		JOptionPane.showMessageDialog(null, "To run this application", "instructions", 1);
 	}
